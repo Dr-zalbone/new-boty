ac55fc2d49d891992a42337b758696679a92de40
Dr-zalbone <dr.zalbone@gmail.com>
2025-12-15 10:54:43 +0200

    added sensors

--- new_boty.ino
+++ new_boty.ino
@@ -1,92 +1,91 @@
-#define ena 5
-#define in1 6
-#define in2 7
-#define in3 8
-#define in4 9
-#define enb 10
+// pins
+#define ena 2
+#define in1 3
+#define in2 4
+#define in3 5
+#define in4 6
+#define enb 7
 
-int const black_line = 100;
-int const on_right = 70;
-int const on_left = 50;
+#define leftSensor A0
+#define centerSensor A1
+#define rightSensor A2
+
+// defenitions for sensors and motor speeds
+int const black_line = 70;
+int const on = 150;
 int const off = 0;
-
-bool right_motor = false;
-bool left_motor = false;
-
-int left;
-int center;
-int right;
+// Values of on(true) and off(false) of the right and left motors
+bool rightMotorValue = false;
+bool leftMotorValue = false;
+// Line folloeing sensor inputs
+int leftInput; 
+int centerInput;
+int rightInput;
 
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(OUTPUT, ena);
-  pinMode(OUTPUT, in1);
-  pinMode(OUTPUT, in2);
-  pinMode(OUTPUT, in3);
-  pinMode(OUTPUT, in4);
-  pinMode(OUTPUT, enb);
+  pinMode(leftSensor, INPUT);
+  pinMode(centerSensor, INPUT);
+  pinMode(rightSensor, INPUT);
+
+
+  pinMode(ena, OUTPUT);
+
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  pinMode(enb, OUTPUT);
   
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
 }
 
 void loop() {
-  left = analogRead(A0);
-  center = analogRead(A1);
-  right = analogRead(A2);
+  leftInput = analogRead(leftSensor);
+  centerInput = analogRead(centerSensor);
+  rightInput = analogRead(rightSensor);
 
-  Serial.print(left);
+  Serial.print(leftInput);
   Serial.print("\t");
-  Serial.print(center);
+  Serial.print(centerInput);
   Serial.print("\t");
-  Serial.println(right);
+  Serial.println(rightInput);
 
-  if (isBlack(left)){
+
+  if (isBlack(leftInput)){
     Serial.print("left motor off ");
-    left_motor = false;
-  }
-  else {
+    leftMotorValue = false;
+  } else {
     Serial.print("left motor on ");
-    left_motor = true;
+    leftMotorValue = true;
   }
-  if (isBlack(right)){
+
+  if (isBlack(rightInput)){
     Serial.print("right motor off ");
-    right_motor = false;
-  }
-  else{
+    rightMotorValue = false;
+  } else {
     Serial.print("right motor on ");
-    right_motor = true;
+    rightMotorValue = true;
   }
+
   Serial.println();
   Serial.println();
   
-  motor(left_motor, right_motor);
+  motor(leftMotorValue, rightMotorValue);
 }
 
 bool isBlack(int value) {
-  if(value < black_line){
-    return true;
-  }
-  else {
-    return false;
-  }
+  if (value < black_line) { return true;  }
+  else                    { return false; }
 }
 
 void motor(int right_motor, int left_motor) {
-  if (right_motor) {
-    analogWrite(ena, on_right);
-  }
-  else {
-    analogWrite(ena, off);
-  }
-  if (left_motor) {
-    analogWrite(enb, on_left);
-  }
-  else {
-    analogWrite(enb, off);
-  }
+
+  if (right_motor) { analogWrite(ena, on);  }
+  else             { analogWrite(ena, off); }
+
+  if (left_motor)  { analogWrite(enb, on);  }
+  else             { analogWrite(enb, off); }
 }
